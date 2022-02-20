794709d7479bd233f41711be87fb9a755e685810
Nicolas Perrin <perrin.isir@gmail.com>
2022-02-20 23:46:27 +0100

    seems more stable when the gradient steps are outside episodes

--- xpag/tools/learn.py
+++ xpag/tools/learn.py
@@ -327,9 +327,9 @@ def learn(
                     # replay_buffer.store_episode(1, episode_argmax, episode_t)
                     replay_buffer.store_episode(num_envs, episode, episode_t)
 
-                    # for _ in range(int(train_ratio * episode_t)):
-                    #     pre_sample = replay_buffer.pre_sample()
-                    #     agent.train(pre_sample, sampler, batch_size)
+                    for _ in range(int(train_ratio * episode_t)):
+                        pre_sample = replay_buffer.pre_sample()
+                        agent.train(pre_sample, sampler, batch_size)
 
                     interval_time, _ = timing()
                     training_time += interval_time/1000.
@@ -422,10 +422,10 @@ def learn(
             )
             # debug()
 
-        if episode_num > 0:
-            for _ in range(train_ratio):
-                pre_sample = replay_buffer.pre_sample()
-                agent.train(pre_sample, sampler, batch_size)
+        # if episode_num > 0:
+        #     for _ in range(train_ratio):
+        #         pre_sample = replay_buffer.pre_sample()
+        #         agent.train(pre_sample, sampler, batch_size)
 
         if save_episode:
             save_ep.update()
