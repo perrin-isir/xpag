575cc79acaab2858762487741414c22751a50062
Nicolas Perrin <perrin.isir@gmail.com>
2022-03-01 21:47:08 +0100

    tests

--- setup.py
+++ setup.py
@@ -33,36 +33,3 @@ setup(
     ],
     license="LICENSE",
 )
-
-setup(
-    name="xpag",
-    version="0.1.0",
-    author="Nicolas Perrin-Gilbert",
-    description="xpag: Exploring Agents",
-    url="https://github.com/perrin-isir/xpag",
-    # packages=find_packages(
-    #     include=[
-    #         "xpag",
-    #         "xpag.*",
-    #         "xpag.agents.*",
-    #         "xpag.agents.jax.*",
-    #         "xpag.buffers.*",
-    #         "xpag.goalsetters.*",
-    #         "xpag.plotting.*",
-    #         "xpag.samplers.*",
-    #         "xpag.tools.*",
-    #     ]
-    # ),
-    packages=find_packages(),
-    install_requires=[
-        "psutil>=5.8.0",
-        "numpy>=1.21.5",
-        "matplotlib>=3.5.1",
-        "joblib>=1.1.0",
-        "gym>=0.22.0",
-        "torch>=1.10.1",
-        "mujoco_py>=2.1.2.14",
-        "IPython>=8.0.1",
-    ],
-    license="LICENSE",
-)
