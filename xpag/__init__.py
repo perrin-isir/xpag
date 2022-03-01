c00f7bcfa1e8b1dd6cbf1ea48d4cdcc6653c60f2
Nicolas Perrin <perrin.isir@gmail.com>
2022-03-01 16:57:33 +0100

    main __init__ updated

--- xpag/__init__.py
+++ xpag/__init__.py
@@ -4,3 +4,9 @@ from . import samplers as sa
 from . import buffers as bf
 from . import agents as ag
 from . import goalsetters as gs
+
+import sys
+
+sys.modules.update(
+    {f"{__name__}.{m}": globals()[m] for m in ["tl", "pl", "sa", "bf", "ag", "gs"]}
+)
