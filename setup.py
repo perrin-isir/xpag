87a877828403cefd1a5d8b3650bc6a9837bb67a2
Nicolas Perrin <perrin.isir@gmail.com>
2022-03-01 15:44:06 +0100

    setting up setup.py

--- setup.py
+++ setup.py
@@ -3,3 +3,13 @@ from setuptools import setup
 # Install with 'pip install -e .'
 
 setup(name="xpag", version="0.1.0", install_requires=[])
+
+setup(
+    name="xpag",
+    version="0.1.0",
+    author="Nicolas Perrin-Gilbert",
+    description="xpag: Exploring Agents",
+    url="https://github.com/perrin-isir/xpag",
+    packages=["xpag"],
+    license="LICENSE",
+)
