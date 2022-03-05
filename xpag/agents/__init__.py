f607be02e36242806783444ef54b5027d9601d84
Nicolas Perrin <perrin.isir@gmail.com>
2022-03-05 19:16:54 +0100

    sac tests

--- xpag/agents/__init__.py
+++ xpag/agents/__init__.py
@@ -1,5 +1,9 @@
-from xpag.agents.agent import (
-    Agent,
-)
-from xpag.agents.jax.sac import SAC
-from xpag.agents.jax.td3 import TD3
+from . import jax
+from . import torch
+
+# from xpag.agents.agent import (
+#     Agent,
+# )
+# from xpag.agents.jax.sac import SAC
+# # from xpag.agents.jax.sac2 import SAC2
+# from xpag.agents.jax.td3 import TD3
--- xpag/agents/jax/__init__.py
+++ xpag/agents/jax/__init__.py
@@ -1,2 +1,4 @@
 from xpag.agents.jax.sac import SAC
+
+# from xpag.agents.jax.sac2 import SAC2
 from xpag.agents.jax.td3 import TD3
