27b6a9ba26824074c56147fa18e998a40baaf782
Nicolas Perrin <perrin.isir@gmail.com>
2022-03-05 21:38:42 +0100

    removed SAC (bug after >1M timesteps)

--- xpag/agents/__init__.py
+++ xpag/agents/__init__.py
@@ -1,9 +1,4 @@
-from . import jax
-from . import torch
-
-# from xpag.agents.agent import (
-#     Agent,
-# )
-# from xpag.agents.jax.sac import SAC
-# # from xpag.agents.jax.sac2 import SAC2
-# from xpag.agents.jax.td3 import TD3
+from xpag.agents.agent import (
+    Agent,
+)
+from xpag.agents.jax.td3 import TD3
--- xpag/agents/jax/__init__.py
+++ xpag/agents/jax/__init__.py
@@ -1,4 +1,1 @@
-from xpag.agents.jax.sac import SAC
-
-# from xpag.agents.jax.sac2 import SAC2
 from xpag.agents.jax.td3 import TD3
