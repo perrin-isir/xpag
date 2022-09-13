4e84cffaea5b11f0d7814ed41b3e81c69a036a80
Nicolas Perrin <perrin.isir@gmail.com>
2022-09-13 19:50:24 +0200

    importing utils functions in __init__ file

--- xpag/tools/__init__.py
+++ xpag/tools/__init__.py
@@ -1,10 +1,13 @@
 from xpag.tools.utils import (
     DataType,
+    get_datatype,
+    datatype_convert,
     reshape,
     hstack,
     logical_or,
     maximum,
-    datatype_convert,
+    squeeze,
+    where,
     get_env_dimensions,
 )
 from xpag.tools.eval import single_rollout_eval
