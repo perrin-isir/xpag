e0776af66a25a6063b0a97a0bebba196d9b03cd6
Nicolas Perrin <perrin.isir@gmail.com>
2023-03-21 16:16:31 +0100

    version 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="xpag",
-    version="0.1.0",
+    version="0.1.1",
     author="Nicolas Perrin-Gilbert",
     description="xpag: Exploring Agents",
     long_description=long_description,
--- xpag/__init__.py
+++ xpag/__init__.py
@@ -6,4 +6,4 @@ from xpag import samplers
 from xpag import agents
 from xpag import setters
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
