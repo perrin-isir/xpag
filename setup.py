e1b710582a930926dfde2801897116b90bf4a16e
Nicolas Perrin-Gilbert <perrin.isir@gmail.com>
2025-03-01 16:37:19 +0100

    correction to worker args

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="xpag",
-    version="0.3.0",  # -version-
+    version="0.3.1",  # -version-
     author="Nicolas Perrin-Gilbert",
     description="xpag: Exploring Agents",
     long_description=long_description,
--- xpag/__init__.py
+++ xpag/__init__.py
@@ -6,4 +6,4 @@ from xpag import samplers
 from xpag import agents
 from xpag import setters
 
-__version__ = "0.3.0"  # -version-
+__version__ = "0.3.1"  # -version-
